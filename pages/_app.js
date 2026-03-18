303b39acca4ea373b90f67da7559a1f7c5eba37e
bpiont1591 <129689696+bpiont1591@users.noreply.github.com>
2026-03-18 23:01:32 +0100

    x

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
